afc4be91738f03aeb8437c4c4a6488a0796a11ed
Felian <ffelian@gmail.com>
2020-10-27 12:22:54 +0300

    Add/Dec/Inc/Set methods now return original value

--- counter.go
+++ counter.go
@@ -11,32 +11,46 @@ type Counter struct {
 // NewCounter returns a new synced counter initialized by initialValue
 func NewCounter(initialValue int) Counter { return Counter{initialValue, sync.Mutex{}} }
 
-// Inc increases counter by 1
-func (c *Counter) Inc() {
+func (c *Counter) dec()      { c.count-- }
+func (c *Counter) inc()      { c.count++ }
+func (c *Counter) add(i int) { c.count += i }
+func (c *Counter) sub(i int) { c.count -= i }
+func (c *Counter) set(i int) { c.count = i }
+
+// Inc increases counter by 1. Returns original value
+func (c *Counter) Inc() int {
 	c.Lock()
-	c.count++
-	c.Unlock()
+	defer c.Unlock()
+	v := c.count
+	c.inc()
+	return v
 }
 
-// Add i to counter
-func (c *Counter) Add(i int) {
+// Add i to counter. Returns original value
+func (c *Counter) Add(i int) int {
 	c.Lock()
-	c.count += i
-	c.Unlock()
+	defer c.Unlock()
+	v := c.count
+	c.add(i)
+	return v
 }
 
-// Dec decreases counter by 1
-func (c *Counter) Dec() {
+// Dec decreases counter by 1. Returns original value
+func (c *Counter) Dec() int {
 	c.Lock()
-	c.count--
-	c.Unlock()
+	defer c.Unlock()
+	v := c.count
+	c.dec()
+	return v
 }
 
-// Set counter to i
-func (c *Counter) Set(i int) {
+// Set counter to i. Returns original value
+func (c *Counter) Set(i int) int {
 	c.Lock()
-	c.count = i
-	c.Unlock()
+	defer c.Unlock()
+	v := c.count
+	c.set(i)
+	return v
 }
 
 // Get returns current counter value
